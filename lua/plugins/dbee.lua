aa24201a4cf8585c6a3004e9c6c1c11a4e54ddce
Aleksei Burmistrov <mr.ehbr@gmail.com>
2025-12-31 14:35:29 +0200

    update config

--- lua/plugins/dbee.lua
+++ lua/plugins/dbee.lua
@@ -5,7 +5,6 @@ return {
 		ft = { "sql" },
 		dependencies = {
 			"MunifTanjim/nui.nvim",
-			-- { "vanducng/cmp-dbee", opts = {} },
 		},
 		keys = {
 			{ "<M-p>", "<cmd>lua require('dbee').toggle()<cr>" },
@@ -15,29 +14,20 @@ return {
 		end,
 		config = function()
 			local dbee = require("dbee")
-			local envSource = require("dbee.sources").EnvSource:new("DBEE_CONNECTIONS")
-			local conns = envSource:load()
+			local TabLayout = require("config.dbee")
 
-			local opts = {
-				default_connection = os.getenv("DBEE_DEFAULT_CONNECTION") or (#conns > 0 and conns[1].name or nil),
+			dbee.setup({
+				window_layout = TabLayout:new(),
 				sources = {
-					envSource,
-					-- // ~/.config/dbee/persistence.json
 					require("dbee.sources").FileSource:new(vim.fn.expand("$HOME/.config/dbee/persistence.json")),
 				},
 				editor = {
 					mappings = {
 						{ key = "<leader>E", mode = "v", action = "run_selection", opts = { desc = "Run selection" } },
-						{
-							key = "<leader>e",
-							mode = "n",
-							action = "run_under_cursor",
-							opts = { desc = "Run under cursor" },
-						},
+						{ key = "<leader>e", mode = "n", action = "run_under_cursor", opts = { desc = "Run under cursor" } },
 					},
 				},
-			}
-			dbee.setup(opts)
+			})
 		end,
 	},
 }
